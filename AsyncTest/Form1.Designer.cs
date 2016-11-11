43a01b3ccc04e2e44e0cca491a0b03185ee11954
kazunori kimura <kazunori.kimura.js@gmail.com>
2016-11-12 00:30:38 +0900

    first commit.

---
+++ AsyncTest/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AsyncTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtTaskNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 48);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(98, 48);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "中断";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtTaskNum
+            // 
+            this.txtTaskNum.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtTaskNum.Location = new System.Drawing.Point(60, 16);
+            this.txtTaskNum.Name = "txtTaskNum";
+            this.txtTaskNum.Size = new System.Drawing.Size(66, 19);
+            this.txtTaskNum.TabIndex = 1;
+            this.txtTaskNum.Text = "3";
+            this.txtTaskNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "タスク数";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(298, 48);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "クリア";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 88);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(361, 172);
+            this.txtLog.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "間隔(ms)";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtInterval.Location = new System.Drawing.Point(228, 16);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(55, 19);
+            this.txtInterval.TabIndex = 7;
+            this.txtInterval.Text = "10000";
+            this.txtInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 272);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTaskNum);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtTaskNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInterval;
+    }
+}
+
---
+++ AsyncTest/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncTest
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// append log delegate
+        /// </summary>
+        /// <param name="text"></param>
+        delegate void AppendLogDelegate(string text);
+
+        /// <summary>
+        /// TaskのCancelTokenSource
+        /// </summary>
+        private CancellationTokenSource tokenSource = null;
+
+        /// <summary>
+        /// Task list
+        /// </summary>
+        private List<Task> taskList = new List<Task>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // タイマー停止状態とする
+            this.timer1.Enabled = false;
+            SetControlsStatus(false);
+        }
+
+        /// <summary>
+        /// 開始
+        /// </summary>
+        private void StartTimer()
+        {
+            try
+            {
+                AppendLog("StartTimer.");
+                // フォームの無効化
+                SetControlsStatus(true);
+                // 時間を取得
+                int interval = int.Parse(this.txtInterval.Text);
+                timer1.Interval = interval;
+                // タイマー開始
+                timer1.Enabled = true;
+
+                // cancel token source
+                tokenSource = new CancellationTokenSource();
+            }
+            catch (Exception e)
+            {
+                this.txtLog.AppendText(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// 中断
+        /// </summary>
+        private void StopTimer()
+        {
+            try
+            {
+                AppendLog("StopTimer.");
+                // タスクのキャンセル
+                tokenSource.Cancel();
+                // timerを停止
+                timer1.Enabled = false;
+                // フォームの有効化
+                SetControlsStatus(false);
+
+                // すべてのタスクがキャンセルされたことを確認
+                while(true)
+                {
+                    Thread.Sleep(300); // 300ms待機
+
+                    foreach (var task in taskList)
+                    {
+                        if (!task.IsCanceled && !task.IsCompleted)
+                        {
+                            continue;
+                        }
+                    }
+                    break;
+                }
+
+                // タスクリストをクリア
+                taskList.Clear();
+                // tokenResourceを削除
+                tokenSource.Dispose();
+            }
+            catch (Exception e)
+            {
+                this.txtLog.AppendText(e.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// 処理開始・中断時にコンポーネントの有効・無効をセット
+        /// </summary>
+        /// <param name="isStarted"></param>
+        private void SetControlsStatus(bool isStarted)
+        {
+            this.txtTaskNum.Enabled = !isStarted;
+            this.txtInterval.Enabled = !isStarted;
+            this.btnStart.Enabled = !isStarted;
+            this.btnCancel.Enabled = isStarted;
+        }
+
+        /// <summary>
+        /// 開始
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        /// <summary>
+        /// 中断
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        /// <summary>
+        /// clear
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtLog.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// テキスト追加
+        /// </summary>
+        /// <param name="text"></param>
+        private void AppendLog(string text)
+        {
+            txtLog.AppendText(string.Format("[{0}] {1}\n",
+                DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff"),
+                text));
+        }
+
+
+        /// <summary>
+        /// ランダム時間停止するだけのタスク
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
+        private Task RandomSleepTask(int taskId)
+        {
+            var token = tokenSource.Token;
+            
+            return Task.Run(async () =>
+            {
+                // Sleep時間を取得 (3,000 ~ 15,000)
+                var rand = new Random(taskId);
+                int waitTime = rand.Next(3000, 15000);
+                // start message
+                string msg = string.Format(@"task{0} started: sleepTime={1}", taskId, waitTime);
+                AppendLogDelegate appendLog = new AppendLogDelegate(AppendLog);
+                this.Invoke(appendLog, new Object[] { msg });
+
+                // sleep
+                await Task.Delay(waitTime, token);
+
+                // cancelされた？
+                token.ThrowIfCancellationRequested();
+
+                msg = string.Format(@"task{0} finished.", taskId);
+                this.Invoke(appendLog, new Object[] { msg });
+            }, token).ContinueWith((t) =>
+            {
+                if (t.IsCanceled)
+                {
+                    // キャンセルされたときの処理
+                    AppendLogDelegate appendLog = new AppendLogDelegate(AppendLog);
+                    this.Invoke(appendLog, new Object[] { string.Format(@"task{0} canceled.", taskId) });
+                }
+            });
+        }
+
+        /// <summary>
+        /// タイマー処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                AppendLog("start timer1_Tick.");
+
+                int taskNum = int.Parse(this.txtTaskNum.Text);
+                for (int i = 0; i < taskNum; i++)
+                {
+                    if (taskList.Count <= i)
+                    {
+                        // タスクを実行
+                        var t = RandomSleepTask(i);
+                        // リストに追加
+                        taskList.Add(t);
+                    }
+                    else
+                    {
+                        var t = taskList[i];
+                        if (!t.Status.Equals(TaskStatus.RanToCompletion))
+                        {
+                            // 前回のタスクが実行中ならスキップする
+                            AppendLog(string.Format("task{0} is running.", i));
+                            continue;
+                        }
+
+                        // タスクを実行
+                        t = RandomSleepTask(i);
+                        // リストに保持
+                        taskList[i] = t;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                AppendLog(exp.StackTrace);
+            }
+        }
+    }
+}
